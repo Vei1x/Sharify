f126c96d5869b42c0bbc7673841ecc9b273a42f4
mig1511 <mig1511@icloud.com>
2021-08-05 10:59:54 +0300

    Updated pom.xml. Added static files and controller for categories

---
+++ src/main/java/com/sharify/dao/TransportDAO.java
@@ -0,0 +1,7 @@
+package com.sharify.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransportDAO {
+}
---
+++ src/main/java/com/sharify/models/Transport.java
@@ -0,0 +1,7 @@
+package com.sharify.models;
+
+public class Transport {
+    private float price;
+    private String model;
+    private int year;
+}
