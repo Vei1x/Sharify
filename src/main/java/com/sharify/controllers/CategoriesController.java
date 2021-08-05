2088e6d2e63fabf482771d92ef53e95f44bafe8a
mig1511 <mig1511@icloud.com>
2021-08-05 11:39:57 +0300

    Added categories

--- src/main/java/com/sharify/controllers/CategoriesController.java
+++ src/main/java/com/sharify/controllers/CategoriesController.java
@@ -1,9 +1,45 @@
 package com.sharify.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/category")
 public class CategoriesController {
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/";
+    }
+
+    @GetMapping("/transport")
+    public String transport(){
+        return "";
+    }
+
+    @GetMapping("/electronics")
+    public String electronics(){
+        return "";
+    }
+
+        @GetMapping("/entertainment")
+    public String entertainment(){
+        return "";
+    }
+
+        @GetMapping("/construction")
+    public String construction(){
+        return "";
+    }
+
+        @GetMapping("/home-stuff")
+    public String homeStuff(){
+        return "";
+    }
+
+        @GetMapping("/other")
+    public String other(){
+        return "";
+    }
 }
